1fedb17e8f6b78f817d3e5718fc712d54c3244c9
Oleh Derenko <oleg.derenko51@gmail.com>
2022-10-12 15:21:57 +0300

    g

--- dtxTwo/script.js
+++ dtxTwo/script.js
@@ -24,3 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const links = document.querySelectorAll(".button-position a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    toggleNavigation();
+  });
+});
