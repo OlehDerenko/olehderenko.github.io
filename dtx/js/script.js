0664277549589104747532fbf456811ecf4064d2
Oleh Derenko <oleg.derenko51@gmail.com>
2023-01-14 15:14:10 +0200

    ff

--- dtx/js/script.js
+++ dtx/js/script.js
@@ -25,3 +25,7 @@ window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
+
+$(document).ready(function () {
+  $("#fullpage").fullpage();
+});
