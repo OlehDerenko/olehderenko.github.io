35cd2c12e72ce95efd9aa6238509bec581df3fc5
Oleh Derenko <oleg.derenko51@gmail.com>
2023-03-25 12:58:06 +0200

    ewq

--- CRM/js/script.js
+++ CRM/js/script.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector(".burger");
 const sidebar = document.querySelector(".sidebar-mobile");
 const body = document.querySelector("body");
+const html = document.querySelector("html");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   sidebar.classList.toggle("active");
   body.classList.toggle("active");
+  html.classList.toggle("active");
 });
