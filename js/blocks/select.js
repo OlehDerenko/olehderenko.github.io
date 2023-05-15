6b802df89da6612e5115add7e4e5a2bf03aab59a
Oleh Derenko <oleg.derenko51@gmail.com>
2023-05-15 15:20:36 +0300

    q

--- js/blocks/select.js
+++ js/blocks/select.js
@@ -5,3 +5,24 @@ const list = document.querySelector(".language__items");
 language.addEventListener("click", () => {
   list.classList.toggle("language__items_active");
 });
+
+const scrollTopGlobalButton = document.querySelector(".button-to-top");
+
+window.addEventListener("scroll", () => {
+  const TOP = 195;
+  if (
+    document.body.scrollTop > TOP ||
+    document.documentElement.scrollTop > TOP
+  ) {
+    scrollTopGlobalButton.style.display = "flex";
+  } else {
+    scrollTopGlobalButton.style.display = "none";
+  }
+});
+
+scrollTopGlobalButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
