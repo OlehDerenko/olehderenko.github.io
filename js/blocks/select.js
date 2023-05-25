b529d3178607d1a60e5897edc3371bc08ddbedb9
Oleh Derenko <oleg.derenko51@gmail.com>
2023-05-25 13:42:02 +0300

    f

--- js/blocks/select.js
+++
@@ -1,7 +0,0 @@
-const lang = document.querySelector("html").getAttribute("lang");
-const language = document.querySelector(".language__item_active");
-const list = document.querySelector(".language__items");
-
-language.addEventListener("click", () => {
-  list.classList.toggle("language__items_active");
-});
